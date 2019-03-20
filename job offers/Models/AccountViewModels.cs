b9ddde52bc8ed18bb8210071da4ac35856e4a5c8
SMAILI abdelkarim <abdelkarimsmaili709@gmail.com>
2019-03-20 17:37:38 +0100

    Add regester page

--- job offers/Models/AccountViewModels.cs
+++ job offers/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
@@ -35,7 +36,7 @@ namespace WebApplication1.Models
 
         [Display(Name = "Mémoriser ce navigateur ?")]
         public bool RememberBrowser { get; set; }
-
+        
         public bool RememberMe { get; set; }
     }
 
@@ -49,35 +50,38 @@ namespace WebApplication1.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Courrier électronique")]
+        [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Mot de passe")]
+        [Display(Name = "password")]
         public string Password { get; set; }
 
-        [Display(Name = "Mémoriser le mot de passe ?")]
+        [Display(Name = "Remember password?")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterViewModel
     {
         [Required]
+        [DisplayName("Username")] 
+        public string UserName { get; set; }
+        [Required]
         [EmailAddress]
-        [Display(Name = "Courrier électronique")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "String {0} must have at least{2} characters.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Mot de passe")]
+        [Display(Name = "password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirmer le mot de passe ")]
-        [Compare("Password", ErrorMessage = "Le mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        [Display(Name = "Confirm password ")]
+        [Compare("Password", ErrorMessage = "The password and the confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 
